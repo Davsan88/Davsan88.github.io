76ab5cade0a811df75719fb86a9cfa19dfb526f0
DavSan <david.sanchez.martinez@outlook.com>
2024-07-04 17:20:05 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,13 @@
   import "../app.css";
 </script>
 
+<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+  
+  <div class="">
+    <button>
+      <i class="fa-solid fa-arrow-up"/>
+    </button>
+  </div>
+
+</div>
 <slot />
