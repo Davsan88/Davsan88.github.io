c9783c76343717495a6b6db4e41aed1fe011deda
DavSan <david.sanchez.martinez@outlook.com>
2024-07-05 17:13:17 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,33 @@
 <script>
   import "../app.css";
   import Footer from "../components/Footer.svelte";
-  import Header from "../components/Header.svelte"
+  import Header from "../components/Header.svelte";
 
   let y;
-  let innerHeight = 0
-  let innerWidth = 0
+  let innerHeight = 0;
+  let innerWidth = 0;
 
   function goTop() {
-    document.body.scrollIntoView()
+    document.body.scrollIntoView();
   }
 </script>
 
-
-<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
-  
-  <div class="">
+<div
+  class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
+>
+  <div
+    class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10]" +
+      (y > 0
+        ? "opacity-full pointer-events-auto"
+        : "opacity-0 pointer-events-none")}
+  >
     <button>
-      <i class="fa-solid fa-arrow-up"/>
+      <i class="fa-solid fa-arrow-up" />
     </button>
   </div>
-  <Header/>
+  <Header />
   <slot />
-  <Footer/>
+  <Footer />
 </div>
 
-<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth/>
-
-
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
