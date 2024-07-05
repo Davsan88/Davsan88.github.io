3cd83617aa3efbf893369ec78f105d391a3b3d6f
DavSan <david.sanchez.martinez@outlook.com>
2024-07-05 14:22:43 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script>
   import "../app.css";
+  import Footer from "../components/Footer.svelte";
+  import Header from "../components/Header.svelte"
+
+  let y;
+  let innerHeight = 0
+  let innerWidth = 0
+
+  function goTop() {
+    document.body.scrollIntoView()
+  }
 </script>
+
 
 <div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
   
@@ -9,6 +20,11 @@
       <i class="fa-solid fa-arrow-up"/>
     </button>
   </div>
-
+  <Header/>
+  <slot />
+  <Footer/>
 </div>
-<slot />
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth/>
+
+
