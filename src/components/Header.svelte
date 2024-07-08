b4d79d57fd149ea24aa854d91320c88f1b2a04ba
DavSan <david.sanchez.martinez@outlook.com>
2024-07-08 20:41:47 +0100

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,7 +7,9 @@
     ];
 </script>
 
-<header>
+<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid' + (
+    y > 0 ? 'py-4 bg-slate-950 border-violet-950' : 'py-6 bg-transparent border-transparent'
+)}>
     <h1>
         <b>David</b> Sanchez
     </h1>
