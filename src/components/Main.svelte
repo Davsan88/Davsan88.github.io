82382c638c2150ba367dd615c6fc6651ba16a73c
DavSan <david.sanchez.martinez@outlook.com>
2024-07-11 18:20:10 +0100

    Update Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -5,7 +5,7 @@
 <main class="flex flex-col flex-1 p-4">
     <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14 bg-orange-800">
         <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10 bg-slate-200 ">
-            <h2 class="font-semibold">Hi! I'm <span class="montserrat text-violet-400">David</span> Sanchez <br/>Software <span class="montserrat text-violet-400">Developer</span>
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">Hi! I'm <span class="montserrat text-violet-400">David</span> Sanchez <br/>Software <span class="montserrat text-violet-400">Developer</span>
             </h2>
         </div>
 
