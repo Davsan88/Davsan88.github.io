e9ee1762777f20489d5b11767617d9ac8cf5b8d5
DavSan <david.sanchez.martinez@outlook.com>
2024-07-11 18:18:08 +0100

    Update Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -3,8 +3,11 @@
 </script>
 
 <main class="flex flex-col flex-1 p-4">
-    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
-        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"></div>
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14 bg-orange-800">
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10 bg-slate-200 ">
+            <h2>Hi! I'm <span class="montserrat text-violet-400">David</span> Sanchez <br/>Software <span class="montserrat text-violet-400">Developer</span>
+            </h2>
+        </div>
 
     </section>
 </main>
