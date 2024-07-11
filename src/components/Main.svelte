66aa2f318f794a0b75f522761eb1b747cfa82f0e
DavSan <david.sanchez.martinez@outlook.com>
2024-07-11 17:53:28 +0100

    Update Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main class="flex flex-col flex-1 p-4">
-    <section>
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
 
     </section>
 </main>
