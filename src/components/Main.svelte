34586efcb617f6a30f54dee83d0ac6778cc8e608
DavSan <david.sanchez.martinez@outlook.com>
2024-07-11 18:58:55 +0100

    Update Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -4,10 +4,10 @@
 <main class="flex flex-col flex-1 p-4">
     <section
         id="introPage"
-        class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14 bg-orange-800"
+        class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
     >
         <div
-            class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10 bg-green-600"
+            class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
         >
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
                 Hi! I'm <span class="montserrat text-violet-400">David</span>
@@ -17,10 +17,15 @@
             <p class="text-base sm:text-lg md:text-xl">
                 My <span class="montserrat text-violet-400">favorite tech</span> includes JavaScript, TailwindCSS & Python!                
             </p>
-            <a class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl montserrat relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
+            <a 
+            class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl montserrat relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer" 
+            href="https://www.linkedin.com/in/david-sanchez-1995ab268/" target="_blank">
                 <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"/>
                 <h4 class="relative z-9">Get in touch &rarr;</h4>
             </a> 
+        </div>
+        <div>
+            <!-- 32:09 -->
         </div>
     </section>
 </main>
