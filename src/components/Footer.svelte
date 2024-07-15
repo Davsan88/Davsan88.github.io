9f4b449c0cb59c4ba3895ac3039650458216bb4a
DavSan <david.sanchez.martinez@outlook.com>
2024-07-15 20:24:32 +0100

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,3 +1,7 @@
-<footer>
-    
+<footer
+    class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+>
+    <p class="px-4 py-2 bg-white text-slate-950 rounded-sm font-medium">
+        Connect with me &darr;
+    </p>
 </footer>
