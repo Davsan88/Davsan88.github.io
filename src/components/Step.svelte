de04e94b752f9380242bd0ee3ed19e81baf1733d
DavSan <david.sanchez.martinez@outlook.com>
2024-07-15 16:20:07 +0100

    Update Step.svelte

--- src/components/Step.svelte
+++ src/components/Step.svelte
@@ -14,11 +14,11 @@
     </div>
     <h3 class="font-medium text-xl sm:text-2xl md:text-3xl">{step.name}</h3>
     <slot />
-    <div class="flex flex-1 justify-between gap-4 items-center">
+    <div class="bg-red-700 flex flex-1 justify-between gap-4 items-center">
         <div
             class="ml-auto cursor-pointer hover:text-slate-950 duration-200 relative after:absolute after:top-0 after:h-0 after:right-full after:w-full after:h-full after:duration-200 hover:after:translate-x-full after:z-[1] overflow-hidden"
         >
-            <p class="relative z-[4]">Go to &rarr</p>
+            <p class="relative z-[4]">Go to &rarr;</p>
         </div>
     </div>
 
