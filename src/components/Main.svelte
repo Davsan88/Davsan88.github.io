f278bad36f31c32079a7e4fe46733f9de6e301cf
DavSan <david.sanchez.martinez@outlook.com>
2024-07-15 17:41:08 +0100

    Update Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -92,13 +92,14 @@
         class="bg-orange-500 py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
     >
         <div 
-            class="flex flex-col gap-2 text-center relative before:relative before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:relative after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700"
+            class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
         >
             <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
-            <h3>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
                 A bit <span class="montserrat text-violet-400">about</span> me.
-            </h3>
-        
+            </h3>        
         </div>
+        <p class="mx-auto montserrat font-semibold text-lg sm:text-xl md:text-2xl">I am . . .</p>
+        
     </section>
 </main>
