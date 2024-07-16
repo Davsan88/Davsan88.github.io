95239769099e1ede34a0f61f27d8a44f9e21eff3
DavSan <david.sanchez.martinez@outlook.com>
2024-07-16 13:05:28 +0100

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,14 +4,30 @@
     <p class="px-4 py-2 bg-white text-slate-950 rounded-sm font-medium">
         Connect with me &darr;
     </p>
-    <div clas="flex flex-col gap-4 items-center justify-center">
+    <div class="flex flex-col gap-4 items-left justify-center">
         <p><b class="pr-2">Email</b> 
-            <a class="text-violet-400" href="mailto: david.sanchez.martinez@outlook.com">david.sanchez.martinez@outlook.com</a></p>
-        <p><b class="pr-2">GitHub</b>
-            <a class="text-violet-400" target="_blank" href="https://github.com/Davsan88">Davsan88
-
+            <a class="text-violet-400" href="mailto: david.sanchez.martinez@outlook.com">david.sanchez.martinez@outlook.com
             </a>
         </p>
-
+        <p><b class="pr-2">GitHub</b>
+            <a class="text-violet-400" target="_blank" href="https://github.com/Davsan88">Davsan88<sup>
+                <span class="pl-0.5">
+                    <i
+                        class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                    />
+                </span>
+            </sup>
+            </a>
+        </p>
+        <p><b class="pr-2">LinkedIn</b>
+            <a class="text-violet-400" target="_blank" href="https://www.linkedin.com/in/david-sanchez-1995ab268/">David Sanchez<sup>
+                <span class="pl-0.5">
+                    <i
+                        class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                    />
+                </span>
+            </sup>
+            </a>
+        </p>
     </div>
 </footer>
