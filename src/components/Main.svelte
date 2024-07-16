409aacd13c710a7a7c5fddbac48c347f52515d0e
DavSan <david.sanchez.martinez@outlook.com>
2024-07-16 13:06:37 +0100

    Update Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -77,14 +77,14 @@
                 work?
             </h3>
         </div>
-        <a
+        <!-- <a
             href=""
             target="_blank"
             class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
         >
             <i class="fa-regular fa-circle-play"></i>
             <p>Watch the video</p>
-        </a>
+        </a> -->
         <div
             class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10"
         >
